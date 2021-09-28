f9e67d12c39cb01e8cfafcdbceaf6b0d0ada8cc6
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2021-09-28 08:31:19 -0500

    Debug

--- client.js
+++ client.js
@@ -5,6 +5,7 @@ id("startmirror").onclick = async () => {
     let api = new TI_USB();
     let device = await api.getDevices();
     device = await api.openDevice();
+    let webcon = new TIWebConnApi(window.alert, window.alert, window.alert, window.alert, window.alert);
     window.alert(device);
     window.api = api;
     window.device = device;
